59572630b4d268101c9ab964b8bd9eaf3ca12667
r_boo <r_boo@192.168.1.71>
2022-11-27 18:02:25 -0800

    Finish Spies On A Train

--- src/_01_Spies_On_A_Train/SpiesOnATrain.java
+++ src/_01_Spies_On_A_Train/SpiesOnATrain.java
@@ -2,63 +2,67 @@ package _01_Spies_On_A_Train;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import _00_Intro_to_Linked_Lists.LinkedList;
 import _00_Intro_to_Linked_Lists.Node;
 
 public class SpiesOnATrain {
 
-    /*
-     * A spy has made off with important intel from your intelligence agency!
-     * You know the spy is somewhere on this train(LinkedList). Your job is to
-     * find the suspect that matches the description given to you by the list of
-     * clues(the array).
-     * 
-     * Walk through the train, questioning each of the passengers about what
-     * they have seen and return the name of the most likely suspect.
-     * 
-     * The results are randomly generated each time so you should have a general
-     * case solution that carefully compares the clues to each passenger's
-     * testimony. Remember to use String methods to break up the passengers'
-     * statements.
-     */
-    String findIntel(LinkedList<TrainCar> train, String[] clues) {
-       String spy = "";
-       ArrayList<String> collected = new ArrayList<String>();
-       ArrayList<String> phone = new ArrayList<String>();
-       ArrayList<String> pie = new ArrayList<String>();
-       ArrayList<String> briefcase = new ArrayList<String>();
-       Node<TrainCar> current = train.getHead();
-    	for (int i = 0; i < train.size(); i++) {
-       	String temp = current.getValue().questionPassenger();
-       	System.out.println(temp);
-       	collected.add(temp);
-       	current = current.getNext();
-	}
-    	StringBuilder sorter;
-    	System.out.println("TESTING");
-    	for (int i = 0; i < collected.size(); i++) {
-			String temp = collected.get(i).toString();
-			sorter = new StringBuilder(temp);
-			if(sorter.indexOf("phone",0)!=-1) {  //if statement sorting out phone clues
-				phone.add(collected.get(i).toString());
-				System.out.println("PHONE ADDED AT "+ i);
+	/*
+	 * A spy has made off with important intel from your intelligence agency! You
+	 * know the spy is somewhere on this train(LinkedList). Your job is to find the
+	 * suspect that matches the description given to you by the list of clues(the
+	 * array).
+	 * 
+	 * Walk through the train, questioning each of the passengers about what they
+	 * have seen and return the name of the most likely suspect.
+	 * 
+	 * The results are randomly generated each time so you should have a general
+	 * case solution that carefully compares the clues to each passenger's
+	 * testimony. Remember to use String methods to break up the passengers'
+	 * statements.
+	 */
+	String findIntel(LinkedList<TrainCar> train, String[] clues) {
+		String spy = "";
+		ArrayList<String> testimonies = new ArrayList<String>();
+		ArrayList<String> keys = new ArrayList<String>();
+		HashMap<String,Integer> suspects = new HashMap<String,Integer>();
+		StringBuilder sorter = new StringBuilder();
+		Node<TrainCar> current = train.getHead();
+		for (int i = 0; i < train.size(); i++) {
+			String temp = current.getValue().questionPassenger();
+			testimonies.add(temp);
+			current = current.getNext();
+		}
+		for (int i = 0; i < testimonies.size(); i++) {
+			sorter = new StringBuilder(testimonies.get(i).toString());
+			for (int j = 0; j < clues.length; j++) {
+				if (sorter.indexOf(clues[j], 0) != -1) {
+					int clueIndex = sorter.indexOf(clues[j], 0);
+					int nameIndex = sorter.indexOf("saw",0)+3;
+					String name = testimonies.get(i).substring(nameIndex, clueIndex).trim();
+					if(suspects.get(name).equals(null)) {
+						suspects.put(name, 1);
+					}
+					else {
+						suspects.replace(name, suspects.get(name)+1);
+					}
+					keys.add(name);
+				}
 			}
-			else if(sorter.indexOf("pie",0)!=-1) {  //if statement sorting out pie clues
-				pie.add(collected.get(i).toString());
-				System.out.println("PIE ADDED AT "+i);
-			}
-			else if(sorter.indexOf("briefcase",0)!=-1) {  //if statement sorting out briefcase clues
-				briefcase.add(collected.get(i).toString());
-				System.out.println("BRIEFCASE ADDED AT "+i);
-			}
-			else {
-				System.out.println("ERROR ADDING AT "+i);
+
+		}
+		int topMatch = 0;
+		for (int i = 0; i < suspects.size(); i++) {
+			if(suspects.get(keys.get(i))>topMatch) {
+				topMatch = suspects.get(keys.get(i));
+				spy = keys.get(i);
 			}
 		}
-    	System.out.println("");
-        return spy;
+		//System.out.println(releventClues.toString());
+		return spy;
 
-    }
+	}
 
 }
